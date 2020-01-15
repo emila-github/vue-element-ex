ef2db601feefb98233edeb106b5897b516a22556
changjianwang <10877098@qq.com>
2020-01-15 11:07:15 +0800

    登录页tab

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
-    <ul>
-      <li>登录</li>
-      <li>注册</li>
-    </ul>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ]
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      data.current = true;
+    }
+  }
 };
 </script>
 
@@ -18,5 +43,25 @@ export default {
 .login {
   background-color: #344a5f;
   height: 100vh;
+
+  .login-wrap {
+    width: 330px;
+    margin: 0 auto;
+    .menu-tab {
+      text-align: center;
+      li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: #fff;
+        border-radius: 2px;
+        cursor: pointer;
+        &.current {
+          background-color: rgba(0, 0, 0, 0.1);
+        }
+      }
+    }
+  }
 }
 </style>

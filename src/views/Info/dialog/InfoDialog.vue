869b5fe155276baf1f2bf6dc94692c5098152d04
changjianwang <10877098@qq.com>
2020-01-23 11:56:10 +0800

    弹窗改为v3写法

--- src/views/Info/dialog/InfoDialog.vue
+++ src/views/Info/dialog/InfoDialog.vue
@@ -6,41 +6,61 @@
     :before-close="handleClose"
     @close="close"
   >
-    <span>这是一段信息</span>
-    <span slot="footer" class="dialog-footer">
+    <el-form :model="form">
+      <el-form-item label="类型" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="标题" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="概况" :label-width="formLabelWidth">
+        <el-input type="textarea" v-model="form.desc"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
       <el-button @click="dialogVisibleFlag = false">取 消</el-button>
       <el-button type="primary" @click="dialogVisibleFlag = false"
         >确 定</el-button
       >
-    </span>
+    </div>
   </el-dialog>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import { reactive, ref, onMounted, watch } from "@vue/composition-api";
 export default {
   name: "InfoDailog",
   props: {
     dialogVisible: {
       type: Boolean,
-      default: false
+      default: true
     }
   },
-  data() {
-    return {
-      // eslint-disable-next-line vue/no-reserved-keys
-      dialogVisibleFlag: false
-    };
-  },
-  watch: {
-    dialogVisible: {
-      // eslint-disable-next-line no-unused-vars
-      handler(newValue, oldValue) {
-        this.dialogVisibleFlag = newValue;
-      }
-    }
-  },
-  methods: {
-    close() {
+  setup(props, { root, emit }) {
+    const dialogVisibleFlag = ref(false);
+    const formLabelWidth = ref("70px");
+
+    const form = reactive({
+      name: "",
+      region: "",
+      date1: "",
+      date2: "",
+      delivery: false,
+      type: [],
+      resource: "",
+      desc: ""
+    });
+
+    // 监听
+    watch(() => {
+      dialogVisibleFlag.value = props.dialogVisible;
+    });
+    // methods
+    const close = () => {
       // 方法一：
       // 回调要做逻辑处理时使用
       // this.$emit("close");
@@ -49,17 +69,31 @@ export default {
       //  不处理逻辑时可以用.sync修饰符来修改父组件值
       //  父组件通过 .sync 修饰符授权子组件更新父组件值
       //  <InfoDialog :dialogVisible.sync="dialogVisible" />
-      this.$emit("update:dialogVisible", false);
-    },
-    handleClose(done) {
-      this.$confirm("确认关闭？")
+      emit("update:dialogVisible", false);
+    };
+    const handleClose = done => {
+      root
+        .$confirm("确认关闭？")
         .then(() => {
           done();
         })
         .catch(() => {});
-    }
+    };
+
+    return {
+      dialogVisibleFlag,
+      formLabelWidth,
+      form,
+      close,
+      handleClose
+    };
   }
 };
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.dialog-footer {
+  display: flex;
+  justify-content: center;
+}
+</style>
